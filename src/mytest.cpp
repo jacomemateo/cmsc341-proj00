e0b74bb8cd2040682dd76d01cc0cd2c67b8e0bc9
mateo <mateo.jacome@hotmail.com>
2024-09-08 21:51:24 -0400

    gonna upload now should work fr

--- src/mytest.cpp
+++ src/mytest.cpp
@@ -26,71 +26,183 @@ public:
             return false;
         }
     }
-
     // 2. Test whether the constructor works correctly for a normal case (supported shape like English board)
     bool testConstructorNormalCase() {
-        // Implementation goes here
-        return false;
-    }
+        Solitaire s1(ENGLISH);
+        Solitaire s2(DIAMOND);
 
+        bool t1 = s1.m_numMarbles == NUMENGLISHMARBLES && s1.m_numRows == ENGLISHSIZE && s1.m_numColumns == ENGLISHSIZE && s1.m_board != nullptr;
+        bool t2 = s2.m_numMarbles == NUMDIAMONDMARBLES && s2.m_numRows == DIAMONDSIZE && s2.m_numColumns == DIAMONDSIZE && s2.m_board != nullptr;
+
+        if(t1 && t2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     // 3. Test whether newBoard(...) works correctly for an error case (e.g., re-initializing an empty object)
     bool testNewBoardErrorCase() {
-        // Implementation goes here
-        return false;
-    }
+        Solitaire s1(NONE);
+        Solitaire s2(FRENCH);
 
+        if(!s1.newBoard() && !s2.newBoard()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     // 4. Test whether newBoard(...) works correctly for a normal case (valid re-initialization)
     bool testNewBoardNormalCase() {
-        // Implementation goes here
-        return false;
-    }
+        Solitaire s1(DIAMOND);
 
+        // Play a few valid moves to change up the board
+        s1.play(pair<int,int>(6,4), pair<int,int>(4,4));
+        s1.play(pair<int,int>(5,2), pair<int,int>(5,4));
+
+        int before_marbles = s1.m_numMarbles;
+        string before_board_state =  s1.board_to_string();
+        int** before_move_address = s1.m_board;
+
+        s1.newBoard();
+
+        int after_marbles = s1.m_numMarbles;
+        string after_board_state =  s1.board_to_string();
+        int** after_move_address = s1.m_board;
+
+        // If the re-initialization worked, the same memory location must've been
+        // kept but the shape must've been reset.
+        if(before_move_address == after_move_address && before_marbles != after_marbles && before_board_state != after_board_state) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     // 5. Test whether changeBoard(...) works correctly when called on the same board shape
     bool testChangeBoardSameShape() {
-        // Implementation goes here
-        return false;
-    }
+        // If it works correctly then the memory address of
+        // the board should be the same but the board state will be different.
+        Solitaire s1(ENGLISH);
+        s1.play(pair(1,3), pair(3,3));
+        s1.play(pair(2,1), pair(2,3));
 
+        int **before_addy = s1.m_board;
+        int before_marble = s1.m_numMarbles;
+        string before_state = s1.board_to_string();
+
+        s1.changeBoard(ENGLISH);
+
+        int **after_addy = s1.m_board;
+        int after_marble = s1.m_numMarbles;
+        string after_state = s1.board_to_string();
+
+        if(before_addy == after_addy &&
+            before_marble != after_marble &&
+            before_state != after_state
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     // 6. Test whether changeBoard(...) works correctly when called on a different board shape
     bool testChangeBoardDifferentShape() {
-        // Implementation goes here
-        return false;
-    }
+        // If it works correctly then the memory address of
+        // the board should be different fr, also everything else should be different too!
+        Solitaire s1(ENGLISH);
 
+        // in this case we don't need to make moves to alter
+        // the board state since changing up the board
+        // will do that for us fr
+
+        int **before_addy = s1.m_board;
+        int before_marble = s1.m_numMarbles;
+        string before_state = s1.board_to_string();
+
+        s1.changeBoard(DIAMOND);
+
+        int **after_addy = s1.m_board;
+        int after_marble = s1.m_numMarbles;
+        string after_state = s1.board_to_string();
+
+        if(before_addy != after_addy &&
+           before_marble != after_marble &&
+           before_state != after_state
+                ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     // 7. Test whether play(...) works correctly for a correct move (normal case)
     bool testPlayNormalMove() {
-        // Implementation goes here
-        return false;
-    }
+        Solitaire s1(ENGLISH);
 
+        // All valid moves
+        if(
+        s1.play(pair(5, 3), pair(3, 3)) &&
+        s1.play(pair(4, 1), pair(4, 3)) &&
+        s1.play(pair(2, 2), pair(4, 2)) &&
+        s1.play(pair(3, 3), pair(5, 3))
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     // 8. Test whether play(...) works correctly for a wrong move (error case)
     bool testPlayErrorMove() {
-        // Implementation goes here
-        return false;
-    }
+        Solitaire s1(DIAMOND);
 
+        // All invalid moves
+        if(
+            !s1.play(pair(0, 0), pair(-1, -1)) &&
+            !s1.play(pair(0, 4), pair(1, 4)) &&
+            !s1.play(pair(4, 4), pair(1032, -9233)) &&
+            !s1.play(pair(5, 2), pair(5, 4))
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     // 9. Test the copy constructor for an error case (copying an empty object)
     bool testCopyConstructorErrorCase() {
-        // Implementation goes here
-        return false;
-    }
+        Solitaire s1(NONE);
+        Solitaire s2(s1);
 
+        // Object is empty
+        if( s2.m_numMarbles == 0 && s2.m_numRows == 0 && s2.m_numColumns == 0 && s2.m_board == nullptr ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     // 10. Test the copy constructor for a normal case (copying a valid object)
     bool testCopyConstructorNormalCase() {
-        // Implementation goes here
-        return false;
-    }
+        Solitaire s1(ENGLISH);
 
-    // 11. Test whether the object remains unchanged when trying to re-initialize an empty object
-    bool testNewBoardDoesNotModifyEmptyObject() {
-        // Implementation goes here
-        return false;
-    }
+        s1.play(pair(5, 3), pair(3, 3));
+        s1.play(pair(4, 1), pair(4, 3));
+        s1.play(pair(2, 2), pair(4, 2));
+        s1.play(pair(3, 3), pair(5, 3));
 
-    // 12. Test whether newBoard(...) returns false when called on an empty object
-    bool testNewBoardReturnsFalseOnEmptyObject() {
-        // Implementation goes here
-        return false;
+        Solitaire s2(s1);
+
+        int original_marbles = s1.m_numMarbles;
+        string original_board_state =  s1.board_to_string();
+        int** original_move_address = s1.m_board;
+
+        int copy_marbles = s2.m_numMarbles;
+        string copy_board_state =  s2.board_to_string();
+        int** copy_move_address = s2.m_board;
+
+        // It should be different memory address but everything
+        // else should be the same fr
+        if(original_move_address != copy_move_address && original_marbles == copy_marbles && original_board_state == copy_board_state) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
 
@@ -106,6 +218,15 @@ void run_test(string name, function<bool()> func) {
 
 int main() {
     Tester t;
-    TEST("testConstructorErrorCase", testConstructorErrorCase);
 
+    TEST("Constructor Error Case", testConstructorErrorCase);
+    TEST("Constructor Normal Case", testConstructorNormalCase);
+    TEST("New Board Error Case", testNewBoardErrorCase);
+    TEST("New Board Normal Case", testNewBoardNormalCase)
+    TEST("Change Board Same Shape", testChangeBoardSameShape);
+    TEST("Change Board Different Shape", testChangeBoardDifferentShape)
+    TEST("Play Normal Move", testPlayNormalMove);
+    TEST("Play Error Move", testPlayErrorMove);
+    TEST("Copy Constructor Error Case", testCopyConstructorErrorCase);
+    TEST("Copy Constructor Normal Case", testCopyConstructorNormalCase);
 }
---
+++ src/solitaire.cpp
@@ -0,0 +1,269 @@
+// UMBC - CMSC 341 - Fall 2024 - Proj0
+#include "solitaire.h"
+
+const pair<int, int> empty_spaces_english[4] = {
+        make_pair(0, 0),
+        make_pair(0, 1),
+        make_pair(1, 0),
+        make_pair(1, 1),
+};
+
+const pair<int, int> empty_spaces_diamond[10] = {
+        make_pair(0, 0),
+        make_pair(0, 1),
+        make_pair(0, 2),
+        make_pair(0, 3),
+        make_pair(1, 0),
+        make_pair(1, 1),
+        make_pair(1, 2),
+        make_pair(2, 0),
+        make_pair(2, 1),
+        make_pair(3, 0),
+};
+
+// Default constructor
+Solitaire::Solitaire() : m_numRows(0), m_numColumns(0), m_board(nullptr), m_shape(NONE), m_numMarbles(0) {}
+
+// Initializes all member variables and it allocates required memory if necessary.
+Solitaire::Solitaire(BOARDSHAPE shape) {
+    init(shape);
+}
+
+Solitaire::Solitaire(const Solitaire & rhs) {
+    m_numRows = rhs.m_numRows;
+    m_numColumns = rhs.m_numColumns;
+    m_shape = rhs.m_shape;
+    m_numMarbles = rhs.m_numMarbles;
+
+
+    if(rhs.m_board == nullptr) {
+        m_board = nullptr;
+    }
+    else {
+        m_board = new int* [m_numRows];
+
+        for(int i=0; i<m_numRows; i++) {
+            m_board[i] = new int[m_numColumns];
+            for(int j=0; j<m_numColumns; j++) {
+                m_board[i][j] = rhs.m_board[i][j];
+            }
+        }
+    }
+}
+
+Solitaire::~Solitaire(){
+    clear();
+}
+
+void Solitaire::clear() {
+    // We have to add this check in case the
+    // user provided a non-supported shape so that
+    // we don't try to deallocate a non-existent address
+    if(m_board != nullptr) {
+        for (int i = 0; i < m_numRows; i++) {
+            delete[] m_board[i];
+        }
+
+        delete[] m_board;
+        m_board = nullptr;
+    }
+
+    // Reset the rest of the variables fr
+    m_numRows = 0;
+    m_numColumns = 0;
+    m_shape = NONE;
+    m_numMarbles = 0;
+}
+
+bool Solitaire::play(pair<int,int> origin, pair<int,int> destination){
+    // Bound checking for the 2d array
+    if(
+        origin.first < 0 || origin.first > m_numColumns ||
+        origin.second < 0 || origin.second > m_numRows ||
+        destination.first < 0 || destination.first > m_numColumns ||
+        destination.second < 0 || destination.second > m_numColumns
+    ) {
+        return false;
+    }
+
+    bool invalid_conditions[4];
+
+    // Destination is empty
+    invalid_conditions[0] = m_board[destination.first][destination.second] != HOLE;
+
+     // Destination is orthogonal
+    invalid_conditions[1] = !(origin.second - destination.second == 0 || origin.first - destination.first == 0);
+
+     // Destination is exactly 2 moves away
+    pair<int, int> direction = make_pair(abs(destination.first - origin.first), abs(destination.second - origin.second));
+    invalid_conditions[2] = !(direction.first == 2 || direction.second == 2);
+
+    // Check that middle point is a marble
+    pair<int, int> middle_point = make_pair( (origin.first+destination.first)/2, (origin.second+destination.second)/2);
+    invalid_conditions[3] = m_board[middle_point.first][middle_point.second] != MARBLE;
+
+    for(bool condition : invalid_conditions) {
+        if(condition) {
+            return false;
+        }
+    }
+
+    // Switch thingies
+    m_board[origin.first][origin.second] = HOLE;
+    m_board[middle_point.first][middle_point.second] = HOLE;
+    m_board[destination.first][destination.second] = MARBLE;
+
+    // Decrement marble fr‼️
+    m_numMarbles--;
+
+    return true;
+}
+
+void Solitaire::changeBoard(BOARDSHAPE shape){
+    // If the provided board shape is the same, just re-initalize
+    if(shape == m_shape) {
+        newBoard();
+    }
+    // Otherwise reset the board
+    else {
+        clear();
+        init(shape);
+    }
+}
+
+bool Solitaire::newBoard(){
+    if(m_board == nullptr) {
+        return false;
+    }
+
+    initializeMemberVariables(m_shape);
+    setupBoard(m_shape);
+    return true;
+}
+
+void Solitaire::dumpBoard() {
+    // this is to facilitate debugging
+    cout << "  ";
+    for (int k=0;k<m_numColumns;k++){
+        cout << k << " ";
+    }
+    cout << endl;
+    for (int i=1;i <= m_numRows;i++){
+        cout << i-1 << " ";
+        for (int j=1;j<=m_numColumns;j++){
+            if (m_board[i-1][j-1] == OUT)
+                cout << "  ";
+            else if (m_board[i-1][j-1] == HOLE)
+                cout << BLACKSTAR;
+            else
+                cout << REDSTAR;
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int Solitaire::reportNumMarbles(){
+    // this is to facilitate debugging 
+    cout << "You have " << m_numMarbles << " marbles to remove!" << endl;
+    return m_numMarbles;
+}
+
+void Solitaire::initializeMemoryBoard() {
+    m_board = new int* [m_numRows];
+
+    for(int i=0; i<m_numRows; i++) {
+        m_board[i] = new int[m_numColumns];
+    }
+}
+
+void Solitaire::populateBoard(const pair<int, int> void_positions[], size_t size) {
+    // Fill the board with marbles since it's the majority of what makes up the board.
+    for(int i=0; i<m_numRows; i++) {
+        m_board[i] = new int[m_numColumns];
+        for(int j=0; j<m_numColumns; j++) {
+            m_board[i][j] = MARBLE;
+        }
+    }
+
+    // Now we simply clear the corners to give the board it's final shape.
+
+    // The valid positions represent the upper left corner of the board,
+    // since all the boards provided are symmetrical we can just reflect them over
+    // The x-axis, y-axis, and both the x and y axies
+    for(size_t i = 0; i<size; i++) {
+        pair<int, int> cc = void_positions[i];
+
+        m_board[cc.first][cc.second] = OUT; // left top
+        m_board[m_numRows - cc.first - 1][cc.second] = OUT; // right top
+        m_board[cc.first][m_numColumns - cc.second - 1] = OUT; // left bottom
+        m_board[m_numRows - cc.first - 1][m_numColumns - cc.second - 1] = OUT; // right bottom
+    }
+
+    // Calculate and set middle hole
+    m_board[m_numRows/2][m_numColumns/2] = HOLE;
+}
+
+bool Solitaire::initializeMemberVariables(BOARDSHAPE shape) {
+    // Self explanatory
+    switch (shape) {
+        case ENGLISH:
+            m_numRows = ENGLISHSIZE;
+            m_numColumns = ENGLISHSIZE;
+            m_numMarbles = NUMENGLISHMARBLES;
+            break;
+        case DIAMOND:
+            m_numRows = DIAMONDSIZE;
+            m_numColumns = DIAMONDSIZE;
+            m_numMarbles = NUMDIAMONDMARBLES;
+            break;
+        default:
+            // If non-supported shape is provided create empty object
+            m_numRows = 0;
+            m_numColumns = 0;
+            m_numMarbles = 0;
+            m_board == nullptr;
+            return false;
+    }
+    return true;
+}
+
+void Solitaire::setupBoard(BOARDSHAPE shape) {
+    size_t size = 0;
+
+    switch (m_shape) {
+        case ENGLISH:
+            size = sizeof(empty_spaces_english)/sizeof(empty_spaces_english[0]);
+            populateBoard(empty_spaces_english, size);
+            break;
+        case DIAMOND:
+            size = sizeof(empty_spaces_diamond)/sizeof(empty_spaces_diamond[0]);
+            populateBoard(empty_spaces_diamond, size);
+            break;
+    }
+}
+
+void Solitaire::init(BOARDSHAPE shape) {
+    m_shape = shape;
+
+    // Functions create empty object if non-supported shape is provided
+    if(initializeMemberVariables(shape)) {
+        initializeMemoryBoard();
+        setupBoard(shape);
+    } // Else we don't have to do anything since a false
+    // result would mean that an invalid shape was passed
+    // so no memory needs to be allocated as `initializeMemberVariables`
+    // already sets board to nullptr if the shape is invalid.
+}
+
+string Solitaire::board_to_string() {
+    string board_string;
+
+    for(int i=0; i<m_numRows; i++) {
+        for(int j=0; j<m_numColumns; j++) {
+            board_string += to_string(m_board[i][j]);
+        }
+    }
+
+    return board_string;
+}
